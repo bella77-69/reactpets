48b9ed5cb1ea7cb5bb6a18942a1614c227016c57
Dalen Chan <dalenchan@dalens-mbp.lan>
2021-05-24 16:39:14 +0100

    All dependencies setup - Babel/EsLint/Prettier

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Pet from "./pet";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
 
 const App = () => {
   return React.createElement("div", {}, [
