b605e092697bfa0b54b208630d927ac450ce24fe
Dalen Chan <dalenchan@dalens-mbp.lan>
2021-05-25 19:01:50 +0100

    Added breed dropdown

--- src/searchParams.js
+++ src/searchParams.js
@@ -5,10 +5,14 @@ const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
 const [location, setLocation] = useState("Manchester");
 const [animal, setAnimal] = useState("");
+const [breed, setBreed] = useState("");
+const breeds = [];
 
     return (
         <div className="search-params">
           <form>
+            
+            {/* Location */}
               <label htmlFor="location">
                   Location
                   <input 
@@ -18,6 +22,8 @@ const [animal, setAnimal] = useState("");
                   value={location} 
                   placeholder="Location"/>
               </label>
+              
+              {/* Animal */}
               <label htmlFor="animal">
                 Animal
                 <select 
@@ -26,11 +32,32 @@ const [animal, setAnimal] = useState("");
                 onChange={e => setAnimal(e.target.value)}
                 onBlur={e => setAnimal(e.target.value)}
                 >
-                  <option/>
+                  <option value=""></option>
                   {
                     ANIMALS.map(animal => (
                       <option value={animal} key={animal}>
                         {animal}
+                      </option>
+
+                    ))
+                  }
+                </select>
+                </label>
+
+                {/* Breed */}
+                <label htmlFor="breed">
+                Breed
+                <select 
+                id="breed" 
+                value={breed} 
+                onChange={e => setBreed(e.target.value)}
+                onBlur={e => setBreed(e.target.value)}
+                >
+                  <option value=""></option>
+                  {
+                    breeds.map(breed => (
+                      <option value={breed} key={breed}>
+                        {breed}
                       </option>
 
                     ))
