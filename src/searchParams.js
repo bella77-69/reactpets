fd155a6fe64358798902deb458397501c0eafcb8
Dalen Chan <dalenchan@dalens-mbp.lan>
2021-05-25 18:52:43 +0100

    Added dropdown menu of animals

--- src/searchParams.js
+++ src/searchParams.js
@@ -1,16 +1,47 @@
-const searchParams = () => {
-    const location = "Manchester"
+import { useState } from 'react';
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+const [location, setLocation] = useState("Manchester");
+const [animal, setAnimal] = useState("");
+
     return (
         <div className="search-params">
           <form>
               <label htmlFor="location">
                   Location
-                  <input id="location" value={location} placeholder="Location"/>
+                  <input 
+                  id="location" 
+                //   Shorthand function 
+                  onChange={(e)  => setLocation(e.target.value)}
+                  value={location} 
+                  placeholder="Location"/>
               </label>
+              <label htmlFor="animal">
+                Animal
+                <select 
+                id="animal" 
+                value={animal} 
+                onChange={e => setAnimal(e.target.value)}
+                onBlur={e => setAnimal(e.target.value)}
+                >
+                  <option/>
+                  {
+                    ANIMALS.map(animal => (
+                      <option value={animal} key={animal}>
+                        {animal}
+                      </option>
+
+                    ))
+                  }
+                </select>
+                </label>
+
               <button>Submit</button>
           </form>
         </div>
     )
 }
 
-export default searchParams; 
+export default SearchParams; 
