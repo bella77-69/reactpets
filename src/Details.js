41e2eaa227899829f2f10ef7e16d0337248a407c
Dalen Chan <dalen.chan@hotmail.com>
2021-05-26 18:54:56 +0100

    Class Component

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,26 @@
-const Details = () => {
-    return <h2>Nice bro</h2>
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Details extends Component {
+    constructor () {
+        super();
+
+        this.state = { loading: true };
+    }
+    async componentDidMount () {
+        const res = await fetch (
+            `http://pets-v2.dec-apis.com/pets?id=${this.props.match.params.id}`
+        );
+        const json = await res.json();
+        this.setState({
+            loading : false
+        })
+    }
+    render () {
+        return (
+            <h2>hi</h2>
+        )
+    }
 }
 
 export default Details;
